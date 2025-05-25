12128971e88ec51209a6e15ffc693fc659fdfec7
João Alexandre <joao.ale25@gmail.com>
2025-05-25 13:20:47 -0300

    Implementação do nome do sistema no inicio da aplicação

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using SistemaGestaoProjetosETarefas.Services;
 using SistemaGestaoProjetosETarefas.Views;
+using Spectre.Console;
 
 namespace SistemaGestaoProjetosETarefas
 {
@@ -8,7 +9,21 @@ namespace SistemaGestaoProjetosETarefas
     {
         static void Main(string[] args)
         {
-              MenuView.MenuPrincipal().Wait();
+            NomeSistema(); // Chama o método para exibir o nome do sistema
+            MenuView.MenuPrincipal().Wait();
+        }
+        public static void NomeSistema()
+        {
+            AnsiConsole.Write
+                (
+                    new FigletText("Sistema de").Centered().Color(Color.Red) // Adiciona o título do sistema
+                );
+            AnsiConsole.Write
+                (
+                    new FigletText("Gestão de Projetos e Tarefas").Centered().Color(Color.Gold1) // Adiciona o título do sistema
+                );
+            Thread.Sleep(2000); // Aguarda 1 segundo
+            AnsiConsole.Clear();
         }
     }
 }
